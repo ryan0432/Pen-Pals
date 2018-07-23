d8b3eabc463d4ff2db53153ee3498d5e01bd4c60
apscodes <apscodes@gmail.com>
2018-07-23 15:16:12 +1000

    Player Interaction and player movement working

---
+++ Unity Project Files/The Pen Pals/Assets/Code/Alex/Player.cs
@@ -0,0 +1,94 @@
+﻿//*!----------------------------!*//
+//*! Programmer: Alex Scicluna 
+//*!----------------------------!*//
+
+
+//*! Using namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    //*! Player Singleton
+    public static Player Instance;
+
+    //*!----------------------------!*//
+    //*!    Private Variables
+    //*!----------------------------!*//
+    #region Private Variables
+
+
+    [Header("Player Object")]
+    [SerializeField]
+    private PlayerInteraction.Player_Type type;
+
+    [HideInInspector]
+    private PlayerInteraction interaction;
+
+
+    #endregion
+
+
+    //*!----------------------------!*//
+    //*!    Public Variables
+    //*!----------------------------!*//
+    #region Public Variables
+
+    //*! Current grid postition
+    public Vector2 current_position;
+
+    //*! Movement distance for player to move
+    [Range(1, 10)]
+    public int movement_distance;
+
+    #endregion
+
+
+    //*!----------------------------!*//
+    //*!    Unity Functions
+    //*!----------------------------!*//
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        type = PlayerInteraction.Player_Type.BLUE;
+
+        interaction = PlayerInteraction.Instance;
+    }
+
+    private void Update()
+    {
+        //*! Check the input of the player based on the type
+        interaction.Check_For_Input(type);
+
+        transform.position = new Vector3(current_position.x, current_position.y, 0);
+
+    }
+        
+
+
+    //*!----------------------------!*//
+    //*!    Custom Functions
+    //*!----------------------------!*//
+
+    //*! Public Access
+    #region Public Functions
+
+ 
+
+    #endregion
+
+
+
+    //*! Private Access
+    #region Private Functions
+
+ 
+    #endregion
+}
--- Unity Project Files/The Pen Pals/Assets/Code/Alex/PlayerInteraction.cs
+++ Unity Project Files/The Pen Pals/Assets/Code/Alex/PlayerInteraction.cs
@@ -14,9 +14,15 @@ public class PlayerInteraction : MonoBehaviour
     //*!    Private Variables
     //*!----------------------------!*//
     #region Private Variables
-    //*! Variables to use atomic style naming convention. 
-    //*! lower_case with each word seperated by an underscore '_'
-    private int m_number;
+
+    
+
+    [SerializeField]
+    private Blocky blocky = new Blocky();
+
+    [SerializeField]
+    private Lionel lionel = new Lionel();
+
     #endregion
 
 
@@ -25,22 +31,44 @@ public class PlayerInteraction : MonoBehaviour
     //*!----------------------------!*//
     #region Public Variables
 
+    public enum Player_Type
+    {
+        RED,
+        BLUE
+    }
+
+    public enum Movement_Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    //*! Player Interaction Singleton
+    public static PlayerInteraction Instance;
+
     #endregion
 
 
     //*!----------------------------!*//
     //*!    Unity Functions
     //*!----------------------------!*//
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
 
     }
-
+ 
     private void Update()
     {
-
+ 
     }
+
 
 
     //*!----------------------------!*//
@@ -50,11 +78,6 @@ public class PlayerInteraction : MonoBehaviour
     //*! Public Access
     #region Public Functions
 
-    //*! Function description
-    public void Function_Public()
-    {
-
-    }
 
     #endregion
 
@@ -62,25 +85,234 @@ public class PlayerInteraction : MonoBehaviour
 
     //*! Private Access
     #region Private Functions
+    
 
-    private void Function_Private()
+    //*! Move the player in the direction based on the input the player gave. Move by set distance
+    private void Move_Player_Direction(Player_Type a_player_Type, Movement_Direction a_movement_Direction)
     {
+        //*! Get a reference to the player object for 'blocky' and 'lionel'
+        blocky.player = Player.Instance;
+        lionel.player = Player.Instance;
 
+        switch (a_player_Type)
+        {
+            case Player_Type.RED:
+
+                //*! Apply the movement to the player - blocky
+                switch (a_movement_Direction)
+                {
+                    case Movement_Direction.UP:
+                        //+- Y Axis
+                        blocky.player.current_position += new Vector2(0, blocky.player.movement_distance);
+                        break;
+                    case Movement_Direction.DOWN:
+                        //-- Y Axis
+                        blocky.player.current_position -= new Vector2(0, blocky.player.movement_distance);
+                        break;
+                    case Movement_Direction.LEFT:
+                        //-- X Axis
+                        blocky.player.current_position -= new Vector2(blocky.player.movement_distance, 0);
+                        break;
+                    case Movement_Direction.RIGHT:
+                        //+- X Axis
+                        blocky.player.current_position += new Vector2(blocky.player.movement_distance, 0);
+                        break;
+                    default:
+                        break;
+                }
+
+                break;
+
+
+            case Player_Type.BLUE:
+
+                //*! Apply the movement to the player - lionel
+                switch (a_movement_Direction)
+                {
+                    case Movement_Direction.UP:
+                        //+- Y Axis
+                        lionel.player.current_position += new Vector2(0, lionel.player.movement_distance);
+                        break;
+                    case Movement_Direction.DOWN:
+                        //-- Y Axis
+                        lionel.player.current_position -= new Vector2(0, lionel.player.movement_distance);
+                        break;
+                    case Movement_Direction.LEFT:
+                        //-- X Axis
+                        lionel.player.current_position -= new Vector2(lionel.player.movement_distance, 0);
+                        break;
+                    case Movement_Direction.RIGHT:
+                        //+- X Axis
+                        lionel.player.current_position += new Vector2(lionel.player.movement_distance, 0);
+                        break;
+                    default:
+                        break;
+                }
+
+                break;
+
+                
+            default:
+                break;
+        }
+    }
+
+    //*! Check if Blocky has inputed something
+    private bool Get_Blocky_Input()
+    {
+        //*! Check if the player is falling, if they are then there is no control in the air.
+        if (blocky.is_falling)
+            return false;
+
+
+        //*! Blocky Input Checks
+        if (Input.GetKeyDown(blocky.Controls.move_up_key))
+        {
+            Move_Player_Direction(Player_Type.RED, Movement_Direction.UP);
+            return true;
+        }
+        else if (Input.GetKeyDown(blocky.Controls.move_down_key))
+        {
+            Move_Player_Direction(Player_Type.RED, Movement_Direction.DOWN);
+            return true;
+        }
+        else if (Input.GetKeyDown(blocky.Controls.move_left_key))
+        {
+            Move_Player_Direction(Player_Type.RED, Movement_Direction.LEFT);
+            return true;
+        }
+        else if (Input.GetKeyDown(blocky.Controls.move_right_key))
+        {
+            Move_Player_Direction(Player_Type.RED, Movement_Direction.RIGHT);
+            return true;
+        }
+
+        return false;
+    }
+
+    //*! Check if Lionel has inputed something
+    private bool Get_Lionel_Input()
+    {
+        //*! Lionel Input Checks
+        if (Input.GetKeyDown(lionel.Controls.move_up_key))
+        {
+            Move_Player_Direction(Player_Type.BLUE, Movement_Direction.UP);
+            return true;
+        }
+        else if (Input.GetKeyDown(lionel.Controls.move_down_key))
+        {
+            Move_Player_Direction(Player_Type.BLUE, Movement_Direction.DOWN);
+            return true;
+        }
+        else if (Input.GetKeyDown(lionel.Controls.move_left_key))
+        {
+            Move_Player_Direction(Player_Type.BLUE, Movement_Direction.LEFT);
+            return true;
+        }
+        else if (Input.GetKeyDown(lionel.Controls.move_right_key))
+        {
+            Move_Player_Direction(Player_Type.BLUE, Movement_Direction.RIGHT);
+            return true;
+        }
+
+        return false;
     }
 
     #endregion
 
 
 
-    //*! Protected Access
+    //*! Inherited Access
     #region Protected Functions
 
-    protected void Function_Protected()
-    {
 
+    //*! Checking for player input
+    public bool Check_For_Input(Player_Type a_player_type)
+    {
+        switch (a_player_type)
+        {
+            case Player_Type.RED:
+                return Get_Blocky_Input();
+                ///break;
+            case Player_Type.BLUE:
+                return Get_Lionel_Input();
+                ///break;
+            default:
+                return false;
+                ///break;
+        }
+
+        ///return false;
     }
 
     #endregion
 
+
+}
+
+
+/// <summary>
+/// Blocky Class Object
+/// </summary>
+[System.Serializable]
+public class Blocky
+{
+    [HideInInspector]
+    //*! Player Class Reference
+    public Player player = Player.Instance;
+
+    [SerializeField]
+    //*! Controls
+    private Movement controls;
+
+    public Movement Controls
+    { get { return controls; } }
+
+
+    //*! Is the player falling - used for disabling the input (No air control)
+    public bool is_falling;
+
+
+
+}
+
+/// <summary>
+/// Lionel Class Object
+/// </summary>
+[System.Serializable]
+public class Lionel
+{
+
+    [HideInInspector]
+    //*! Player Class Reference
+    public Player player = Player.Instance;
+
+    [SerializeField]
+    //*! Controls
+    private Movement controls;
+
+    public Movement Controls
+    { get { return controls; } }
+
+}
+
+/// <summary>
+/// Universal Controls for both players
+/// </summary>
+[System.Serializable]
+public struct Movement
+{
+
+    //*! Controls
+    public KeyCode move_up_key;
+    public KeyCode move_down_key;
+    public KeyCode move_right_key;
+    public KeyCode move_left_key;
+
+    public static void Check(PlayerInteraction.Player_Type a_player_type)
+    {
+        //PlayerInteraction.Check_For_Input(a_player_type);
+    }
+ 
 
 }
