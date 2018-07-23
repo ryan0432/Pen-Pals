cbb80dc968a24072fde334d4186238057e0b5777
apscodes <apscodes@gmail.com>
2018-07-23 11:48:44 +1000

    Added in a template code sheet to reference from and follow. Also started the player interaction script

---
+++ Unity Project Files/The Pen Pals/Assets/Code/Template Code Sheet/Template Code Sheet.cs
@@ -0,0 +1,87 @@
+﻿//*!----------------------------!*//
+//*! Programmer: Alex Scicluna || Ryan Chung
+//*!----------------------------!*//
+
+
+//*! Using namespaces
+using UnityEngine;
+
+
+public class TemplateCodeSheet : MonoBehaviour
+{
+
+    //*!----------------------------!*//
+    //*!    Private Variables
+    //*!----------------------------!*//
+    #region Private Variables
+    //*! Variables to use atomic style naming convention. 
+    //*! lower_case with each word seperated by an underscore '_'
+    private int m_number;
+    #endregion
+
+
+    //*!----------------------------!*//
+    //*!    Public Variables
+    //*!----------------------------!*//
+    #region Public Variables
+
+    #endregion
+
+
+    //*!----------------------------!*//
+    //*!    Unity Functions
+    //*!----------------------------!*//
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+    //*!----------------------------!*//
+    //*!    Custom Functions
+    //*!----------------------------!*//
+
+    //*! Public Access
+    #region Public Functions
+
+    //*! Function description
+    public void Function_Public()
+    {
+
+    }
+
+    #endregion
+
+
+
+    //*! Private Access
+    #region Private Functions
+
+    private void Function_Private()
+    {
+
+    }
+
+    #endregion
+
+
+
+    //*! Protected Access
+    #region Protected Functions
+
+    protected void Function_Protected()
+    {
+
+    }
+
+    #endregion
+
+
+
+}
