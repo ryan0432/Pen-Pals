d0b89c6b09871c867e69fec8c8b417eb87c9a2c4
RYAN-LION\RyaN <ryan0432@gmail.com>
2018-08-20 01:17:27 +1000

    Basic node base map is 80% ready. Need to fix some mathmatical issues and set up the edge and its Gizmos on Monday. Then Alex is good to go testing player entities.

--- Unity Project Files/The Pen Pals/Assets/Code/Ryan/Temp_Node_Map.cs
+++ Unity Project Files/The Pen Pals/Assets/Code/Ryan/Temp_Node_Map.cs
@@ -9,16 +9,23 @@ using UnityEngine;
 
 public class Temp_Node_Map : MonoBehaviour
 {
-    private List<Node> nodes;
-    private List<Edge> edges;
+    [Range(1, 500)]
     public int row;
+    [Range(1, 500)]
     public int col;
+    [Range(0.1f, 1.0f)]
+    public float handle_size;
+
+    private Node[,] bl_nodes;
+    private List<Edge> bl_edges;
+    private Node[,] li_nodes;
+    private List<Edge> li_edges;
+
     // Use this for initialization
     void Start ()
     {
-       nodes = new List<Node>();
-       edges = new List<Edge>();
-	}
+        Initialize_Graph();
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -28,31 +35,121 @@ public class Temp_Node_Map : MonoBehaviour
 	}
 
     //*!----------------------------!*//
-    //*!    Unity Functions
+    //*!    Custom Functions
     //*!----------------------------!*//
 
-    private void OnDrawGizmos()
+    //*! Initialize the graphs for Block and Line entities
+    private void Initialize_Graph()
     {
-        for (int i = 0; i < nodes.Count; ++i)
-        {
+        bl_nodes = new Node[row - 1, col - 1];
+        bl_edges = new List<Edge>();
+        li_nodes = new Node[row, col];
+        li_edges = new List<Edge>();
 
+        //*! Setup Block nodes by the number of row and col
+        for (int i = 0; i < row - 1; ++i)
+        {
+            for (int j = 0; j < col - 1; ++j)
+            {
+                bl_nodes[i, j] = new Node();
+                bl_nodes[i, j].Position = new Vector3(i + 0.5f, j + 0.5f);
+                bl_nodes[i, j].Node_Size = handle_size;
+                bl_nodes[i, j].Can_UP = true;
+                bl_nodes[i, j].Can_DN = true;
+                bl_nodes[i, j].Can_LFT = true;
+                bl_nodes[i, j].Can_RGT = true;
+            }
+        }
+
+        //*! Setup Line nodes by the number of row and col
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                li_nodes[i, j] = new Node();
+                li_nodes[i, j].Position = new Vector3(i, j);
+                li_nodes[i, j].Node_Size = handle_size;
+                li_nodes[i, j].Can_UP = true;
+                li_nodes[i, j].Can_DN = true;
+                li_nodes[i, j].Can_LFT = true;
+                li_nodes[i, j].Can_RGT = true;
+            }
+        }
+
+        //*! Iterate from the buttom left node of Block 2D array to check UP & RIGHT direction's traversability.
+        //*! If a direction's traversability is true, create an edge between them. Otherwise don't.
+        for (int i = 0; i < bl_nodes.GetUpperBound(0) - 1; ++i)
+        {
+            for (int j = 0; j < bl_nodes.GetUpperBound(1) - 1; ++j)
+            {
+                if (bl_nodes[i, j].Can_UP && bl_nodes[i, j + 1].Can_DN)
+                {
+                    Edge new_edge = new Edge();
+                    new_edge.Start_Node = bl_nodes[i, j];
+                    new_edge.End_Node = bl_nodes[i, j + 1];
+                    bl_edges.Add(new_edge);
+                }
+
+                if (bl_nodes[i, j].Can_RGT && bl_nodes[i + 1, j].Can_LFT)
+                {
+                    Edge new_edge = new Edge();
+                    new_edge.Start_Node = bl_nodes[i, j];
+                    new_edge.End_Node = bl_nodes[i + 1, j];
+                    bl_edges.Add(new_edge);
+                }
+            }
         }
     }
 
-    //*! Structs for map elements [Node] and [Edge] 
-    struct Node
+    //*!----------------------------!*//
+    //*!    Unity Functions
+    //*!----------------------------!*//
+
+    void OnDrawGizmos()
     {
-        Vector3 position;
-        float node_size;
-        bool Can_UP;
-        bool Can_DN;
-        bool Can_LFT;
-        bool Can_RGT;
+        if (!Application.isPlaying) return;
+
+        for (int i = 0; i < bl_nodes.GetUpperBound(0); ++i)
+        {
+            for (int j = 0; j < bl_nodes.GetUpperBound(1); ++j)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere(bl_nodes[i,j].Position, handle_size);
+            }
+        }
+
+        for (int i = 0; i < li_nodes.GetUpperBound(0); ++i)
+        {
+            for (int j = 0; j < li_nodes.GetUpperBound(1); ++j)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(li_nodes[i, j].Position, handle_size);
+            }
+        }
     }
 
-    struct Edge
+
+    //*!----------------------------!*//
+    //*!    Custom Subclass
+    //*!----------------------------!*//
+
+    //*! Structs for map elements [Node] and [Edge] 
+    class Node
     {
-        Node start_node;
-        Node end_node;
+        //*! Getter, Setter of Node members
+        public Vector3 Position { get; set; }
+        public float Node_Size { get; set; }
+        public bool Can_UP { get; set; }
+        public bool Can_DN { get; set; }
+        public bool Can_LFT { get; set; }
+        public bool Can_RGT { get; set; }
+
+    }
+
+    class Edge
+    {
+        //*! Getter, Setter of Edge members
+        public Node Start_Node { get; set; }
+        public Node End_Node { get; set; }
     }
 }
