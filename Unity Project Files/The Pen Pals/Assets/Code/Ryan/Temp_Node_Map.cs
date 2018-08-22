943ed7e1fd73846497db5949263f1a054f141c75
AIE\s171050 <ryan0432@gmail.com>
2018-08-22 17:13:26 +1000

    1. Edited node class to have it hold neighbor nodes' reference. 2. Four booleans UP, DOWN, LEFT, RIGHT has been set automatically to checking if its direction is holding a node reference or null. No need to switch it manually anymore. 3. Nodes intinialization process is tested and examined, no problem.

--- Unity Project Files/The Pen Pals/Assets/Code/Ryan/Temp_Node_Map.cs
+++ Unity Project Files/The Pen Pals/Assets/Code/Ryan/Temp_Node_Map.cs
@@ -19,10 +19,9 @@ public class Temp_Node_Map : MonoBehaviour
     public int col;
     [Range(0.1f, 1.0f)]
     public float handle_size;
-    [HideInInspector]
     public Node[,] BL_Nodes { get { return bl_nodes; } set { bl_nodes = value; } }
-    [HideInInspector]
     public Node[,] LI_Nodes { get { return li_nodes; } set { li_nodes = value; } }
+
 
     //*!----------------------------!*//
     //*!    Private Variables
@@ -31,9 +30,13 @@ public class Temp_Node_Map : MonoBehaviour
     /*- Note: The reason to use 2D array for nodes is it is easier to find 
               connections between neighbor nodes than a list. As for edges
               only has two node refs, therefore using a list. -*/
+    [HideInInspector]
     private Node[,] bl_nodes;
+    [HideInInspector]
     private List<Edge> bl_edges;
+    [HideInInspector]
     private Node[,] li_nodes;
+    [HideInInspector]
     private List<Edge> li_edges;
 
     // Use this for initialization
@@ -62,78 +65,201 @@ public class Temp_Node_Map : MonoBehaviour
         li_nodes = new Node[row, col];
         li_edges = new List<Edge>();
 
-        //*! Setup Block nodes by the number of row and col
+        //*! ------------------ Old method WITHOUT neighbor nodes ------------------ !*//
+
+        //*! Setup Block nodes by the number of row and col (currently disabled)
         #region Setup [Block] Nodes and limit the boarders' Traversability
+        //for (int i = 0; i < bl_nodes.GetLength(0); ++i)
+        //{
+        //    for (int j = 0; j < bl_nodes.GetLength(1); ++j)
+        //    {
+        //        bl_nodes[i, j] = new Node();
+        //        bl_nodes[i, j].Position = new Vector3(i + 0.5f, j + 0.5f);
+        //        bl_nodes[i, j].Node_Size = handle_size;
+
+        //        bl_nodes[i, j].Can_UP = true;
+        //        bl_nodes[i, j].Can_DN = true;
+        //        bl_nodes[i, j].Can_LFT = true;
+        //        bl_nodes[i, j].Can_RGT = true;
+
+        //        if (i == bl_nodes.GetLowerBound(0))
+        //        {
+        //            bl_nodes[i, j].Can_LFT = false;
+        //        }
+
+        //        if (i == bl_nodes.GetUpperBound(0))
+        //        {
+        //            bl_nodes[i, j].Can_RGT = false;
+        //        }
+
+        //        if (j == bl_nodes.GetLowerBound(1))
+        //        {
+        //            bl_nodes[i, j].Can_DN = false;
+        //        }
+
+        //        if (j == bl_nodes.GetUpperBound(1))
+        //        {
+        //            bl_nodes[i, j].Can_UP = false;
+        //        }
+        //    }
+        //}
+        #endregion
+
+        //*! Setup Line nodes by the number of row and col (currently disabled)
+        #region Setup [Line] Nodes and limit the boarders' Traversability
+        //for (int i = 0; i < li_nodes.GetLength(0); ++i)
+        //{
+        //    for (int j = 0; j < li_nodes.GetLength(1); ++j)
+        //    {
+        //        li_nodes[i, j] = new Node();
+        //        li_nodes[i, j].Position = new Vector3(i, j);
+        //        li_nodes[i, j].Node_Size = handle_size;
+        //        li_nodes[i, j].Can_UP = true;
+        //        li_nodes[i, j].Can_DN = true;
+        //        li_nodes[i, j].Can_LFT = true;
+        //        li_nodes[i, j].Can_RGT = true;
+
+        //        if (i == li_nodes.GetLowerBound(0))
+        //        {
+        //            li_nodes[i, j].Can_LFT = false;
+        //        }
+
+        //        if (i == li_nodes.GetUpperBound(0))
+        //        {
+        //            li_nodes[i, j].Can_RGT = false;
+        //        }
+
+        //        if (j == li_nodes.GetLowerBound(1))
+        //        {
+        //            li_nodes[i, j].Can_DN = false;
+        //        }
+
+        //        if (j == li_nodes.GetUpperBound(1))
+        //        {
+        //            li_nodes[i, j].Can_UP = false;
+        //        }
+        //    }
+        //}
+        #endregion
+
+        //*! ------------------ New method WITH neighbor nodes ------------------ !*//
+
+        //*! Setup [Block] and [Line] nodes by the number of row and col
+        #region Setup [Block] Nodes and Preset Neighbor Nodes to Null
         for (int i = 0; i < bl_nodes.GetLength(0); ++i)
         {
             for (int j = 0; j < bl_nodes.GetLength(1); ++j)
             {
-                bl_nodes[i, j] = new Node();
+                Node new_node = new Node();
+                bl_nodes[i, j] = new_node;
                 bl_nodes[i, j].Position = new Vector3(i + 0.5f, j + 0.5f);
                 bl_nodes[i, j].Node_Size = handle_size;
-                
-                bl_nodes[i, j].Can_UP = true;
-                bl_nodes[i, j].Can_DN = true;
-                bl_nodes[i, j].Can_LFT = true;
-                bl_nodes[i, j].Can_RGT = true;
-
-                if (i == bl_nodes.GetLowerBound(0))
-                {
-                    bl_nodes[i, j].Can_LFT = false;
-                }
-
-                if (i == bl_nodes.GetUpperBound(0))
-                {
-                    bl_nodes[i, j].Can_RGT = false;
-                }
-
-                if (j == bl_nodes.GetLowerBound(1))
-                {
-                    bl_nodes[i, j].Can_DN = false;
-                }
-
-                if (j == bl_nodes.GetUpperBound(1))
-                {
-                    bl_nodes[i, j].Can_UP = false;
-                }
+                bl_nodes[i, j].UP_NODE = null;
+                bl_nodes[i, j].DN_NODE = null;
+                bl_nodes[i, j].LFT_NODE = null;
+                bl_nodes[i, j].RGT_NODE = null;
             }
         }
         #endregion
 
-        //*! Setup Line nodes by the number of row and col
-        #region Setup [Line] Nodes and limit the boarders' Traversability
+        #region Setup [Block] Nodes and Neighbor Nodes' linkage in UP & RGT Direction
+        for (int i = 0; i < bl_nodes.GetUpperBound(0); ++i)
+        {
+            for (int j = 0; j < bl_nodes.GetUpperBound(1); ++j)
+            {
+                bl_nodes[i, j].UP_NODE = bl_nodes[i, j + 1];
+                bl_nodes[i, j].RGT_NODE = bl_nodes[i + 1, j];
+            }
+        }
+
+        for (int i = 0; i < bl_nodes.GetUpperBound(0); ++i)
+        {
+            bl_nodes[i, bl_nodes.GetUpperBound(1)].RGT_NODE = bl_nodes[i + 1, bl_nodes.GetUpperBound(1)];
+        }
+
+        for (int i = 0; i < bl_nodes.GetUpperBound(1); ++i)
+        {
+            bl_nodes[bl_nodes.GetUpperBound(0), i].UP_NODE = bl_nodes[bl_nodes.GetUpperBound(0), i + 1];
+        }
+        #endregion
+
+        #region Setup [Block] Nodes and Neighbor Nodes' linkage in DN & LFT Direction
+        for (int i = bl_nodes.GetUpperBound(0); i > 0; --i)
+        {
+            for (int j = bl_nodes.GetUpperBound(1); j > 0; --j)
+            {
+                bl_nodes[i, j].DN_NODE = bl_nodes[i, j - 1];
+                bl_nodes[i, j].LFT_NODE = bl_nodes[i - 1, j];
+            }
+        }
+
+        for (int i = bl_nodes.GetUpperBound(0); i > 0; --i)
+        {
+            bl_nodes[i, bl_nodes.GetLowerBound(1)].LFT_NODE = bl_nodes[i - 1, bl_nodes.GetLowerBound(1)];
+        }
+
+        for (int i = bl_nodes.GetUpperBound(1); i > 0; --i)
+        {
+            bl_nodes[bl_nodes.GetLowerBound(0), i].DN_NODE = bl_nodes[bl_nodes.GetLowerBound(0), i - 1];
+        }
+        #endregion
+
+        #region Setup [Line] Nodes and Preset Neighbor Nodes to Null
         for (int i = 0; i < li_nodes.GetLength(0); ++i)
         {
             for (int j = 0; j < li_nodes.GetLength(1); ++j)
             {
-                li_nodes[i, j] = new Node();
+                Node new_node = new Node();
+                li_nodes[i, j] = new_node;
                 li_nodes[i, j].Position = new Vector3(i, j);
                 li_nodes[i, j].Node_Size = handle_size;
-                li_nodes[i, j].Can_UP = true;
-                li_nodes[i, j].Can_DN = true;
-                li_nodes[i, j].Can_LFT = true;
-                li_nodes[i, j].Can_RGT = true;
-
-                if (i == li_nodes.GetLowerBound(0))
-                {
-                    li_nodes[i, j].Can_LFT = false;
-                }
-
-                if (i == li_nodes.GetUpperBound(0))
-                {
-                    li_nodes[i, j].Can_RGT = false;
-                }
-
-                if (j == li_nodes.GetLowerBound(1))
-                {
-                    li_nodes[i, j].Can_DN = false;
-                }
-
-                if (j == li_nodes.GetUpperBound(1))
-                {
-                    li_nodes[i, j].Can_UP = false;
-                }
+                li_nodes[i, j].UP_NODE = null;
+                li_nodes[i, j].DN_NODE = null;
+                li_nodes[i, j].LFT_NODE = null;
+                li_nodes[i, j].RGT_NODE = null;
             }
+        }
+        #endregion
+
+        #region Setup [Line] Nodes and Neighbor Nodes' linkage in UP & RGT Direction
+        for (int i = 0; i < li_nodes.GetUpperBound(0); ++i)
+        {
+            for (int j = 0; j < li_nodes.GetUpperBound(1); ++j)
+            {
+                li_nodes[i, j].UP_NODE = li_nodes[i, j + 1];
+                li_nodes[i, j].RGT_NODE = li_nodes[i + 1, j];
+            }
+        }
+
+        for (int i = 0; i < li_nodes.GetUpperBound(0); ++i)
+        {
+            li_nodes[i, li_nodes.GetUpperBound(1)].RGT_NODE = li_nodes[i + 1, li_nodes.GetUpperBound(1)];
+        }
+
+        for (int i = 0; i < li_nodes.GetUpperBound(1); ++i)
+        {
+            li_nodes[li_nodes.GetUpperBound(0), i].UP_NODE = li_nodes[li_nodes.GetUpperBound(0), i + 1];
+        }
+        #endregion
+
+        #region Setup [Line] Nodes and Neighbor Nodes' linkage in DN & LFT Direction
+        for (int i = li_nodes.GetUpperBound(0); i > 0; --i)
+        {
+            for (int j = li_nodes.GetUpperBound(1); j > 0; --j)
+            {
+                li_nodes[i, j].DN_NODE = li_nodes[i, j - 1];
+                li_nodes[i, j].LFT_NODE = li_nodes[i - 1, j];
+            }
+        }
+
+        for (int i = li_nodes.GetUpperBound(0); i > 0; --i)
+        {
+            li_nodes[i, li_nodes.GetLowerBound(1)].LFT_NODE = li_nodes[i - 1, li_nodes.GetLowerBound(1)];
+        }
+
+        for (int i = li_nodes.GetUpperBound(1); i > 0; --i)
+        {
+            li_nodes[li_nodes.GetLowerBound(0), i].DN_NODE = li_nodes[li_nodes.GetLowerBound(0), i - 1];
         }
         #endregion
 
@@ -327,6 +453,10 @@ public class Temp_Node_Map : MonoBehaviour
     //*! Debug the graph. Display all member values of nodes'
     private void Debug_Graph()
     {
+        Debug.Log("------ Start [Block] Node Array Info ------");
+        Debug.Log("Block Row Number: " + bl_nodes.GetLength(0));
+        Debug.Log("Block Col Number: " + bl_nodes.GetLength(1));
+
         for (int i = 0; i < bl_nodes.GetLength(0); ++i)
         {
             for (int j = 0; j < bl_nodes.GetLength(1); ++j)
@@ -339,9 +469,11 @@ public class Temp_Node_Map : MonoBehaviour
             }
         }
 
-        Debug.Log("Block Row Number: " + bl_nodes.GetLength(0));
-        Debug.Log("Block Col Number: " + bl_nodes.GetLength(1));
+        Debug.Log("------ End [Block] Node Array Info ------");
 
+        Debug.Log("------ Start [Line] Node Array Info ------");
+        Debug.Log("Line Row Number: " + li_nodes.GetLength(0));
+        Debug.Log("Line Col Number: " + li_nodes.GetLength(1));
         for (int i = 0; i < li_nodes.GetLength(0); ++i)
         {
             for (int j = 0; j < li_nodes.GetLength(1); ++j)
@@ -353,9 +485,8 @@ public class Temp_Node_Map : MonoBehaviour
                 Debug.Log("Line Node" + "[" + i + " , " + j + "]" + " C_RGT: " + "( " + li_nodes[i, j].Can_RGT + " )");
             }
         }
+        Debug.Log("------ End [Line] Node Array Info ------");
 
-        Debug.Log("Line Row Number: " + li_nodes.GetLength(0));
-        Debug.Log("Line Col Number: " + li_nodes.GetLength(1));
     }
 
 
@@ -405,7 +536,7 @@ public class Temp_Node_Map : MonoBehaviour
             }
         }
 
-        //*!Iterate through both Block and Line's node arrays and draw the nodes
+        //*! Iterate through both Block and Line's node arrays and draw the nodes
         for (int i = 0; i < li_nodes.GetLength(0); ++i)
         {
             for (int j = 0; j < li_nodes.GetLength(1); ++j)
@@ -472,10 +603,19 @@ public class Temp_Node_Map : MonoBehaviour
         //*! Getter, Setter of Node members
         public Vector3 Position { get; set; }
         public float Node_Size { get; set; }
-        public bool Can_UP { get; set; }
-        public bool Can_DN { get; set; }
-        public bool Can_LFT { get; set; }
-        public bool Can_RGT { get; set; }
+
+        //*! Getter, Setter of Node members
+        public bool Can_UP { get { return UP_NODE != null; } }
+        public bool Can_DN { get { return DN_NODE != null; } }
+        public bool Can_LFT { get { return LFT_NODE != null; } }
+        public bool Can_RGT { get { return RGT_NODE != null; } }
+
+        //*! Neighbor Node reference holder
+        public Node UP_NODE { get; set; }
+        public Node DN_NODE { get; set; }
+        public Node LFT_NODE { get; set; }
+        public Node RGT_NODE { get; set; }
+
     }
 
     [System.Serializable]
