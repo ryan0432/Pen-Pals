0989556e417eb3eea33db06a1f4247077b933c65
RYAN-LION\RyaN <ryan0432@gmail.com>
2018-09-01 00:47:51 +1000

    Finish experementing new method (GameObject instantiation) to layout node gizmos. Very little latency detected unless selecting all nodes. Will keep experimenting the other method (DrawMesh) to see if it is going to cause latency. Will consider drawMesh for nodes, only instantiate edges due to the fact that the designer only need to change edges' attribute.

--- Unity Project Files/The Pen Pals/Assets/Code/Ryan/Pencil_Case.cs
+++ Unity Project Files/The Pen Pals/Assets/Code/Ryan/Pencil_Case.cs
@@ -22,33 +22,25 @@ public class Pencil_Case : MonoBehaviour
 
     #region Public Vars
 
+    public int init_row;
+    public int init_col;
+
     [Range(2, 50)]
     public int row;
     [Range(2, 50)]
     public int col;
+
+
     [Range(0.1f, 1.0f)]
     public float handle_size;
-    public Node[,] BL_Nodes { get { return bl_nodes; } set { bl_nodes = value; } }
+    public Node[,] BL_Nodes { get; set; }
     public Node[,] LI_Nodes { get { return li_nodes; } set { li_nodes = value; } }
-    public List<Edge> BL_Edges { get { return bl_edges; } set { bl_edges = value; } }
+    public List<Edge> BL_Edges { get; set; }
     public List<Edge> LI_Edges { get { return li_edges; } set { li_edges = value; } }
 
     #endregion
-
-
-    //*!----------------------------!*//
-    //*!    Private Variables
-    //*!----------------------------!*//
-
     #region Private Vars
 
-    /*- Note: The reason to use 2D array for nodes is it is easier to find 
-              connections between neighbor nodes than a list. As for edges
-              only has two node refs, therefore using a list. -*/
-    //[HideInInspector]
-    private Node[,] bl_nodes;
-    //[HideInInspector]
-    private List<Edge> bl_edges;
     [HideInInspector]
     private Node[,] li_nodes;
     [HideInInspector]
@@ -56,6 +48,21 @@ public class Pencil_Case : MonoBehaviour
 
     [SerializeField]
     private GameObject bl_node_giz;
+    [SerializeField]
+    private GameObject bl_arrw_giz;
+    [SerializeField]
+    private GameObject li_node_giz;
+    [SerializeField]
+    private GameObject li_arrw_giz;
+
+    [SerializeField]
+    private Mesh node_giz;
+    [SerializeField]
+    private Mesh arrw_giz;
+    [SerializeField]
+    private Material bl_giz_mat;
+    [SerializeField]
+    private Material li_giz_mat;
 
     #endregion
 
@@ -102,68 +109,68 @@ public class Pencil_Case : MonoBehaviour
     {
         //*! Populate the graph with [Node] default construction
         #region Setup instances for [Block] and [Line] Nodes and Edges
-        bl_nodes = new Node[row - 1, col - 1];
-        bl_edges = new List<Edge>();
+        BL_Nodes = new Node[row - 1, col - 1];
+        BL_Edges = new List<Edge>();
         li_nodes = new Node[row, col];
         li_edges = new List<Edge>();
         #endregion
 
         #region Setup [Block] Nodes and Preset Neighbor Nodes to Null
-        for (int i = 0; i < bl_nodes.GetLength(0); ++i)
+        for (int i = 0; i < BL_Nodes.GetLength(0); ++i)
         {
-            for (int j = 0; j < bl_nodes.GetLength(1); ++j)
+            for (int j = 0; j < BL_Nodes.GetLength(1); ++j)
             {
                 Node new_node = new Node();
-                bl_nodes[i, j] = new_node;
-                bl_nodes[i, j].Position = new Vector3(i + 0.5f, j + 0.5f);
-                bl_nodes[i, j].Node_Size = handle_size;
-                bl_nodes[i, j].UP_NODE = null;
-                bl_nodes[i, j].DN_NODE = null;
-                bl_nodes[i, j].LFT_NODE = null;
-                bl_nodes[i, j].RGT_NODE = null;
+                BL_Nodes[i, j] = new_node;
+                BL_Nodes[i, j].Position = new Vector3(i + 0.5f, j + 0.5f);
+                BL_Nodes[i, j].Node_Size = handle_size;
+                BL_Nodes[i, j].UP_NODE = null;
+                BL_Nodes[i, j].DN_NODE = null;
+                BL_Nodes[i, j].LFT_NODE = null;
+                BL_Nodes[i, j].RGT_NODE = null;
             }
         }
         #endregion
 
         #region Setup [Block] Nodes and Neighbor Nodes' linkage in UP & RGT Direction
-        for (int i = 0; i < bl_nodes.GetUpperBound(0); ++i)
+        for (int i = 0; i < BL_Nodes.GetUpperBound(0); ++i)
         {
-            for (int j = 0; j < bl_nodes.GetUpperBound(1); ++j)
+            for (int j = 0; j < BL_Nodes.GetUpperBound(1); ++j)
             {
-                bl_nodes[i, j].UP_NODE = bl_nodes[i, j + 1];
-                bl_nodes[i, j].RGT_NODE = bl_nodes[i + 1, j];
+                BL_Nodes[i, j].UP_NODE = BL_Nodes[i, j + 1];
+                BL_Nodes[i, j].RGT_NODE = BL_Nodes[i + 1, j];
             }
         }
 
-        for (int i = 0; i < bl_nodes.GetUpperBound(0); ++i)
+        for (int i = 0; i < BL_Nodes.GetUpperBound(0); ++i)
         {
-            bl_nodes[i, bl_nodes.GetUpperBound(1)].RGT_NODE = bl_nodes[i + 1, bl_nodes.GetUpperBound(1)];
+            BL_Nodes[i, BL_Nodes.GetUpperBound(1)].RGT_NODE = BL_Nodes[i + 1, BL_Nodes.GetUpperBound(1)];
         }
 
-        for (int i = 0; i < bl_nodes.GetUpperBound(1); ++i)
+        for (int i = 0; i < BL_Nodes.GetUpperBound(1); ++i)
         {
-            bl_nodes[bl_nodes.GetUpperBound(0), i].UP_NODE = bl_nodes[bl_nodes.GetUpperBound(0), i + 1];
+            BL_Nodes[BL_Nodes.GetUpperBound(0), i].UP_NODE = BL_Nodes[BL_Nodes.GetUpperBound(0), i + 1];
         }
         #endregion
 
         #region Setup [Block] Nodes and Neighbor Nodes' linkage in DN & LFT Direction
-        for (int i = bl_nodes.GetUpperBound(0); i > 0; --i)
+        for (int i = BL_Nodes.GetUpperBound(0); i > 0; --i)
         {
-            for (int j = bl_nodes.GetUpperBound(1); j > 0; --j)
+            for (int j = BL_Nodes.GetUpperBound(1); j > 0; --j)
             {
-                bl_nodes[i, j].DN_NODE = bl_nodes[i, j - 1];
-                bl_nodes[i, j].LFT_NODE = bl_nodes[i - 1, j];
+                BL_Nodes[i, j].DN_NODE = BL_Nodes[i, j - 1];
+                BL_Nodes[i, j].LFT_NODE = BL_Nodes[i - 1, j];
             }
         }
 
-        for (int i = bl_nodes.GetUpperBound(0); i > 0; --i)
+        for (int i = BL_Nodes.GetUpperBound(0); i > 0; --i)
         {
-            bl_nodes[i, bl_nodes.GetLowerBound(1)].LFT_NODE = bl_nodes[i - 1, bl_nodes.GetLowerBound(1)];
+            BL_Nodes[i, BL_Nodes.GetLowerBound(1)].LFT_NODE = BL_Nodes[i - 1, BL_Nodes.GetLowerBound(1)];
         }
 
-        for (int i = bl_nodes.GetUpperBound(1); i > 0; --i)
+        for (int i = BL_Nodes.GetUpperBound(1); i > 0; --i)
         {
-            bl_nodes[bl_nodes.GetLowerBound(0), i].DN_NODE = bl_nodes[bl_nodes.GetLowerBound(0), i - 1];
+            BL_Nodes[BL_Nodes.GetLowerBound(0), i].DN_NODE = BL_Nodes[BL_Nodes.GetLowerBound(0), i - 1];
         }
         #endregion
 
@@ -229,47 +236,47 @@ public class Pencil_Case : MonoBehaviour
         //*! Iterate from the buttom left node of [Block] 2D array to check [UP] & [RGT] direction's traversability.
         //*! If a direction's traversability is true, create an edge between them. Otherwise don't.
         #region Setup [Block] Edges in UP & RGT Direction
-        for (int i = 0; i < bl_nodes.GetUpperBound(0); ++i)
+        for (int i = 0; i < BL_Nodes.GetUpperBound(0); ++i)
         {
-            for (int j = 0; j < bl_nodes.GetUpperBound(1); ++j)
+            for (int j = 0; j < BL_Nodes.GetUpperBound(1); ++j)
             {
-                if (bl_nodes[i, j].Can_UP)
+                if (BL_Nodes[i, j].Can_UP)
                 {
                     Edge new_edge = new Edge();
-                    new_edge.Start_Node = bl_nodes[i, j];
-                    new_edge.End_Node = bl_nodes[i, j + 1];
-                    bl_edges.Add(new_edge);
+                    new_edge.Start_Node = BL_Nodes[i, j];
+                    new_edge.End_Node = BL_Nodes[i, j + 1];
+                    BL_Edges.Add(new_edge);
                 }
 
-                if (bl_nodes[i, j].Can_RGT)
+                if (BL_Nodes[i, j].Can_RGT)
                 {
                     Edge new_edge = new Edge();
-                    new_edge.Start_Node = bl_nodes[i, j];
-                    new_edge.End_Node = bl_nodes[i + 1, j];
-                    bl_edges.Add(new_edge);
+                    new_edge.Start_Node = BL_Nodes[i, j];
+                    new_edge.End_Node = BL_Nodes[i + 1, j];
+                    BL_Edges.Add(new_edge);
                 }
             }
         }
 
-        for (int i = 0; i < bl_nodes.GetUpperBound(0); ++i)
+        for (int i = 0; i < BL_Nodes.GetUpperBound(0); ++i)
         {
-            if (bl_nodes[i, bl_nodes.GetUpperBound(1)].Can_RGT)
+            if (BL_Nodes[i, BL_Nodes.GetUpperBound(1)].Can_RGT)
             {
                 Edge new_edge = new Edge();
-                new_edge.Start_Node = bl_nodes[i, bl_nodes.GetUpperBound(1)];
-                new_edge.End_Node = bl_nodes[i + 1, bl_nodes.GetUpperBound(1)];
-                bl_edges.Add(new_edge);
+                new_edge.Start_Node = BL_Nodes[i, BL_Nodes.GetUpperBound(1)];
+                new_edge.End_Node = BL_Nodes[i + 1, BL_Nodes.GetUpperBound(1)];
+                BL_Edges.Add(new_edge);
             }
         }
 
-        for (int i = 0; i < bl_nodes.GetUpperBound(1); ++i)
+        for (int i = 0; i < BL_Nodes.GetUpperBound(1); ++i)
         {
-            if (bl_nodes[bl_nodes.GetUpperBound(0), i].Can_UP)
+            if (BL_Nodes[BL_Nodes.GetUpperBound(0), i].Can_UP)
             {
                 Edge new_edge = new Edge();
-                new_edge.Start_Node = bl_nodes[bl_nodes.GetUpperBound(0), i];
-                new_edge.End_Node = bl_nodes[bl_nodes.GetUpperBound(0), i + 1];
-                bl_edges.Add(new_edge);
+                new_edge.Start_Node = BL_Nodes[BL_Nodes.GetUpperBound(0), i];
+                new_edge.End_Node = BL_Nodes[BL_Nodes.GetUpperBound(0), i + 1];
+                BL_Edges.Add(new_edge);
             }
         }
         #endregion
@@ -277,47 +284,47 @@ public class Pencil_Case : MonoBehaviour
         //*! Iterate from the upper right node of [Block] 2D array to check [DN] & [LFT] direction's traversability.
         //*! If a direction's traversability is true, create an edge between them. Otherwise don't.
         #region Setup [Block] Edges in DN & LFT Direction
-        for (int i = bl_nodes.GetUpperBound(0); i > 0; --i)
+        for (int i = BL_Nodes.GetUpperBound(0); i > 0; --i)
         {
-            for (int j = bl_nodes.GetUpperBound(1); j > 0; --j)
+            for (int j = BL_Nodes.GetUpperBound(1); j > 0; --j)
             {
-                if (bl_nodes[i, j].Can_DN)
+                if (BL_Nodes[i, j].Can_DN)
                 {
                     Edge new_edge = new Edge();
-                    new_edge.Start_Node = bl_nodes[i, j];
-                    new_edge.End_Node = bl_nodes[i, j - 1];
-                    bl_edges.Add(new_edge);
+                    new_edge.Start_Node = BL_Nodes[i, j];
+                    new_edge.End_Node = BL_Nodes[i, j - 1];
+                    BL_Edges.Add(new_edge);
                 }
 
-                if (bl_nodes[i, j].Can_LFT)
+                if (BL_Nodes[i, j].Can_LFT)
                 {
                     Edge new_edge = new Edge();
-                    new_edge.Start_Node = bl_nodes[i, j];
-                    new_edge.End_Node = bl_nodes[i - 1, j];
-                    bl_edges.Add(new_edge);
+                    new_edge.Start_Node = BL_Nodes[i, j];
+                    new_edge.End_Node = BL_Nodes[i - 1, j];
+                    BL_Edges.Add(new_edge);
                 }
             }
         }
 
-        for (int i = bl_nodes.GetUpperBound(0); i > 0; --i)
+        for (int i = BL_Nodes.GetUpperBound(0); i > 0; --i)
         {
-            if (bl_nodes[i, bl_nodes.GetLowerBound(1)].Can_LFT)
+            if (BL_Nodes[i, BL_Nodes.GetLowerBound(1)].Can_LFT)
             {
                 Edge new_edge = new Edge();
-                new_edge.Start_Node = bl_nodes[i, bl_nodes.GetLowerBound(1)];
-                new_edge.End_Node = bl_nodes[i - 1, bl_nodes.GetLowerBound(1)];
-                bl_edges.Add(new_edge);
+                new_edge.Start_Node = BL_Nodes[i, BL_Nodes.GetLowerBound(1)];
+                new_edge.End_Node = BL_Nodes[i - 1, BL_Nodes.GetLowerBound(1)];
+                BL_Edges.Add(new_edge);
             }
         }
 
-        for (int i = bl_nodes.GetUpperBound(1); i > 0; --i)
+        for (int i = BL_Nodes.GetUpperBound(1); i > 0; --i)
         {
-            if (bl_nodes[bl_nodes.GetLowerBound(0), i].Can_DN)
+            if (BL_Nodes[BL_Nodes.GetLowerBound(0), i].Can_DN)
             {
                 Edge new_edge = new Edge();
-                new_edge.Start_Node = bl_nodes[bl_nodes.GetLowerBound(0), i];
-                new_edge.End_Node = bl_nodes[bl_nodes.GetLowerBound(0), i - 1];
-                bl_edges.Add(new_edge);
+                new_edge.Start_Node = BL_Nodes[BL_Nodes.GetLowerBound(0), i];
+                new_edge.End_Node = BL_Nodes[BL_Nodes.GetLowerBound(0), i - 1];
+                BL_Edges.Add(new_edge);
             }
         }
         #endregion
@@ -422,11 +429,71 @@ public class Pencil_Case : MonoBehaviour
     //*! Render the graph by instantiating [GameObjects]
     private void Render_Graph()
     {
-        for (int i = 0; i < bl_nodes.GetLength(0); ++i)
+        float gizmos_spacing = handle_size * 0.5f;
+
+        for (int i = 0; i < BL_Nodes.GetLength(0); ++i)
         {
-            for (int j = 0; j < bl_nodes.GetLength(1); ++j)
+            for (int j = 0; j < BL_Nodes.GetLength(1); ++j)
             {
-                Instantiate(bl_node_giz, bl_nodes[i,j].Position,Quaternion.identity, transform);
+                GameObject newNodeGiz = Instantiate(bl_node_giz, BL_Nodes[i, j].Position, Quaternion.identity, transform.GetChild(0));
+                newNodeGiz.transform.localScale *= handle_size;
+
+                if (BL_Nodes[i, j].Can_UP)
+                {
+                    Vector3 arrowPos = BL_Nodes[i, j].Position + new Vector3(0, gizmos_spacing, 0);
+                    GameObject newNodeArrwGiz = Instantiate(bl_arrw_giz, arrowPos, Quaternion.identity, newNodeGiz.transform);
+                }
+
+                if (BL_Nodes[i, j].Can_DN)
+                {
+                    Vector3 arrowPos = BL_Nodes[i, j].Position + new Vector3(0, -gizmos_spacing, 0);
+                    GameObject newNodeArrwGiz = Instantiate(bl_arrw_giz, arrowPos, Quaternion.AngleAxis(180f, Vector3.forward), newNodeGiz.transform);
+                }
+
+                if (BL_Nodes[i, j].Can_LFT)
+                {
+                    Vector3 arrowPos = BL_Nodes[i, j].Position + new Vector3(-gizmos_spacing, 0, 0);
+                    GameObject newNodeArrwGiz = Instantiate(bl_arrw_giz, arrowPos, Quaternion.AngleAxis(90f, Vector3.forward), newNodeGiz.transform);
+                }
+
+                if (BL_Nodes[i, j].Can_RGT)
+                {
+                    Vector3 arrowPos = BL_Nodes[i, j].Position + new Vector3(gizmos_spacing, 0, 0);
+                    GameObject newNodeArrwGiz = Instantiate(bl_arrw_giz, arrowPos, Quaternion.AngleAxis(270f, Vector3.forward), newNodeGiz.transform);
+                }
+            }
+        }
+
+        for (int i = 0; i < LI_Nodes.GetLength(0); ++i)
+        {
+            for (int j = 0; j < LI_Nodes.GetLength(1); ++j)
+            {
+                GameObject newNodeGiz = Instantiate(li_node_giz, LI_Nodes[i, j].Position, Quaternion.identity, transform.GetChild(1));
+                newNodeGiz.transform.localScale *= handle_size;
+
+                if (LI_Nodes[i, j].Can_UP)
+                {
+                    Vector3 arrowPos = LI_Nodes[i, j].Position + new Vector3(0, gizmos_spacing, 0);
+                    GameObject newNodeArrwGiz = Instantiate(li_arrw_giz, arrowPos, Quaternion.identity, newNodeGiz.transform);
+                }
+
+                if (LI_Nodes[i, j].Can_DN)
+                {
+                    Vector3 arrowPos = LI_Nodes[i, j].Position + new Vector3(0, -gizmos_spacing, 0);
+                    GameObject newNodeArrwGiz = Instantiate(li_arrw_giz, arrowPos, Quaternion.AngleAxis(180f, Vector3.forward), newNodeGiz.transform);
+                }
+
+                if (LI_Nodes[i, j].Can_LFT)
+                {
+                    Vector3 arrowPos = LI_Nodes[i, j].Position + new Vector3(-gizmos_spacing, 0, 0);
+                    GameObject newNodeArrwGiz = Instantiate(li_arrw_giz, arrowPos, Quaternion.AngleAxis(90f, Vector3.forward), newNodeGiz.transform);
+                }
+
+                if (LI_Nodes[i, j].Can_RGT)
+                {
+                    Vector3 arrowPos = LI_Nodes[i, j].Position + new Vector3(gizmos_spacing, 0, 0);
+                    GameObject newNodeArrwGiz = Instantiate(li_arrw_giz, arrowPos, Quaternion.AngleAxis(270f, Vector3.forward), newNodeGiz.transform);
+                }
             }
         }
     }
@@ -434,9 +501,27 @@ public class Pencil_Case : MonoBehaviour
     //*! Destroys instantiated node prefabs
     private void Update_Graph()
     {
-        for (int i = transform.childCount ; i > 0; --i)
+        bool is_BL_Node_Gizmos_List_Empty = (transform.GetChild(0).childCount < 1);
+        bool is_LI_Node_Gizmos_List_Empty = (transform.GetChild(1).childCount < 1);
+
+        if (!is_BL_Node_Gizmos_List_Empty)
         {
-            DestroyImmediate(transform.GetChild(0).gameObject, true);
+            for (int i = transform.GetChild(0).childCount; i > 0; --i)
+            {
+                DestroyImmediate(transform.GetChild(0).GetChild(0).gameObject, true);
+                
+            }
+            Debug.Log("Clear [Block] Node Gizmos");
+        }
+
+        if (!is_LI_Node_Gizmos_List_Empty)
+        {
+            for (int i = transform.GetChild(1).childCount; i > 0; --i)
+            {
+                DestroyImmediate(transform.GetChild(1).GetChild(0).gameObject, true);
+
+            }
+            Debug.Log("Clear [Line] Node Gizmos");
         }
     }
 
